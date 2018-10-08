1037ee05da9d69d36c7fda2d5b3a631a95b2bc6b
Gregor von Laszewski <laszewski@gmail.com>
2018-10-08 07:22:47 -0400

    code cleanup

--- Sensors/ADS1115.py
+++ Sensors/ADS1115.py
@@ -20,50 +20,49 @@
 # THE SOFTWARE.
 import time
 
-
 # Register and other configuration values:
-ADS1x15_DEFAULT_ADDRESS        = 0x48
-ADS1x15_POINTER_CONVERSION     = 0x00
-ADS1x15_POINTER_CONFIG         = 0x01
-ADS1x15_POINTER_LOW_THRESHOLD  = 0x02
+ADS1x15_DEFAULT_ADDRESS = 0x48
+ADS1x15_POINTER_CONVERSION = 0x00
+ADS1x15_POINTER_CONFIG = 0x01
+ADS1x15_POINTER_LOW_THRESHOLD = 0x02
 ADS1x15_POINTER_HIGH_THRESHOLD = 0x03
-ADS1x15_CONFIG_OS_SINGLE       = 0x8000
-ADS1x15_CONFIG_MUX_OFFSET      = 12
+ADS1x15_CONFIG_OS_SINGLE = 0x8000
+ADS1x15_CONFIG_MUX_OFFSET = 12
 # Maping of gain values to config register values.
 ADS1x15_CONFIG_GAIN = {
-    2/3: 0x0000,
-    1:   0x0200,
-    2:   0x0400,
-    4:   0x0600,
-    8:   0x0800,
-    16:  0x0A00
+    2 / 3: 0x0000,
+    1: 0x0200,
+    2: 0x0400,
+    4: 0x0600,
+    8: 0x0800,
+    16: 0x0A00
 }
-ADS1x15_CONFIG_MODE_CONTINUOUS  = 0x0000
-ADS1x15_CONFIG_MODE_SINGLE      = 0x0100
+ADS1x15_CONFIG_MODE_CONTINUOUS = 0x0000
+ADS1x15_CONFIG_MODE_SINGLE = 0x0100
 # Mapping of data/sample rate to config register values for ADS1015 (faster).
 ADS1015_CONFIG_DR = {
-    128:   0x0000,
-    250:   0x0020,
-    490:   0x0040,
-    920:   0x0060,
-    1600:  0x0080,
-    2400:  0x00A0,
-    3300:  0x00C0
+    128: 0x0000,
+    250: 0x0020,
+    490: 0x0040,
+    920: 0x0060,
+    1600: 0x0080,
+    2400: 0x00A0,
+    3300: 0x00C0
 }
 # Mapping of data/sample rate to config register values for ADS1115 (slower).
 ADS1115_CONFIG_DR = {
-    8:    0x0000,
-    16:   0x0020,
-    32:   0x0040,
-    64:   0x0060,
-    128:  0x0080,
-    250:  0x00A0,
-    475:  0x00C0,
-    860:  0x00E0
+    8: 0x0000,
+    16: 0x0020,
+    32: 0x0040,
+    64: 0x0060,
+    128: 0x0080,
+    250: 0x00A0,
+    475: 0x00C0,
+    860: 0x00E0
 }
-ADS1x15_CONFIG_COMP_WINDOW      = 0x0010
+ADS1x15_CONFIG_COMP_WINDOW = 0x0010
 ADS1x15_CONFIG_COMP_ACTIVE_HIGH = 0x0008
-ADS1x15_CONFIG_COMP_LATCHING    = 0x0004
+ADS1x15_CONFIG_COMP_LATCHING = 0x0004
 ADS1x15_CONFIG_COMP_QUE = {
     1: 0x0000,
     2: 0x0001,
@@ -128,7 +127,7 @@ class ADS1x15(object):
         self._device.writeList(ADS1x15_POINTER_CONFIG, [(config >> 8) & 0xFF, config & 0xFF])
         # Wait for the ADC sample to finish based on the sample rate plus a
         # small offset to be sure (0.1 millisecond).
-        time.sleep(1.0/data_rate+0.0001)
+        time.sleep(1.0 / data_rate + 0.0001)
         # Retrieve the result.
         result = self._device.readList(ADS1x15_POINTER_CONVERSION, 2)
         return self._conversion_value(result[1], result[0])
@@ -177,7 +176,7 @@ class ADS1x15(object):
         self._device.writeList(ADS1x15_POINTER_CONFIG, [(config >> 8) & 0xFF, config & 0xFF])
         # Wait for the ADC sample to finish based on the sample rate plus a
         # small offset to be sure (0.1 millisecond).
-        time.sleep(1.0/data_rate+0.0001)
+        time.sleep(1.0 / data_rate + 0.0001)
         # Retrieve the result.
         result = self._device.readList(ADS1x15_POINTER_CONVERSION, 2)
         return self._conversion_value(result[1], result[0])
@@ -329,7 +328,7 @@ class ADS1115(ADS1x15):
 
     def _conversion_value(self, low, high):
         value = ((high & 0xFF) << 8) | (low & 0xFF)
-#        print "Read", low, hex(low), high, hex(high), value/10000.0
+        #        print "Read", low, hex(low), high, hex(high), value/10000.0
         # Check for sign bit and turn into a negative value if set.
         if value & 0x8000 != 0:
             value -= 1 << 16
@@ -359,19 +358,21 @@ class ADS1015(ADS1x15):
             value -= 1 << 12
         return value
 
+
 def test():
     adc = ADS1115()
     GAIN = 1
     print('Reading ADS1x15 values, press Ctrl-C to quit...')
     # Print nice channel column headers.
     print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*range(4)))
-    print('-' * 37)    
+    print('-' * 37)
     while True:
-        values = [0]*4        
+        values = [0] * 4
         for x in range(4):
             values[x] = adc.read_adc(x, gain=GAIN)
-            print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*values))            
+            print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*values))
             time.sleep(0.5)
-            
+
+
 if __name__ == "__main__":
-    test()    
+    test()
--- Sensors/GPS.py
+++ Sensors/GPS.py
@@ -1,4 +1,4 @@
-'''
+"""
 USB GPS handler, simple serial reader with parsing
 Note: Only one consumer can use this object because serial ports cannot be shared
 Sentence: RMC
@@ -16,8 +16,8 @@ Sentence: RMC
 Author: Howard Webb
 Date: 2018/10/04
 
-'''
-
+"""
+from __future__ import print_function
 import serial
 
 
@@ -25,62 +25,76 @@ class GPS(object):
     ''' GPS object '''
 
     def __init__(self, callback, port='/dev/ttyUSB0'):
-        '''get callback and setup GPS'''
+        """
+        Get the GPS data and print it with the callback function
+        :param callback: The callback function
+        :param port: The port for the GPS
+        """
         self._callback = callback
         self._port = None
         try:
             self._port = serial.Serial(port, baudrate=4800, timeout=1)
-        except Exception:
-            print Exception
+        except Exception as e:
+            print(e)
 
     def watch(self):
-        '''For each new record, parse it and pass it to the callback'''
+        """
+        infinite loop to watch the GPS. For each new record, parse it and pass it to the callback
+        :return:
+        """
         while True:
-            self.getSentence()
+            self.get()
 
-    def getSentence(self):
+    # noinspection PyUnusedLocal
+    def get(self):
+        """
+        reads the gPS value and prints it with the callback function
+        :return: None
+        """
+
         if self._port is None:
-            values = {'name':None, 'data':None}
+            values = {'name': None, 'data': None}
             self._callback(values)
             return
 
-        if(self._port.inWaiting()>0):
-            ''' Get sentence without blocking '''                
-            new_data = self._port.readline().decode('ascii', errors='replace')            
+        if self._port.inWaiting() > 0:
+            ''' Get sentence without blocking '''
+            new_data = self._port.readline().decode('ascii', errors='replace')
             if new_data:
                 values = []
                 sentence = new_data.split(',')
-                if sentence[0]=='$GPRMC':
+                if sentence[0] == '$GPRMC':
                     date = '20' + sentence[9][4:6] + '/' + sentence[9][2:4] + '/' + sentence[9][0:2]
-                    time = sentence[1][0:2]+':'+sentence[1][2:4]+':'+sentence[1][4:6]
-                    timestamp = date + 'T'+time
-                    DD = int(float(sentence[3])/100)
-                    SS = float(sentence[3]) - DD*100
-                    LatDec = DD + SS/60
-                    if sentence[4]=='S':
+                    time = sentence[1][0:2] + ':' + sentence[1][2:4] + ':' + sentence[1][4:6]
+                    timestamp = date + 'T' + time
+                    DD = int(float(sentence[3]) / 100)
+                    SS = float(sentence[3]) - DD * 100
+                    LatDec = DD + SS / 60
+                    if sentence[4] == 'S':
                         LatDec = LatDec * -1
 
-                    DD = int(float(sentence[5])/100)
-                    SS = float(sentence[5]) - DD*100
-                    LonDec = DD + SS/60
-                    if sentence[6]=='W':
+                    DD = int(float(sentence[5]) / 100)
+                    SS = float(sentence[5]) - DD * 100
+                    LonDec = DD + SS / 60
+                    if sentence[6] == 'W':
                         LonDec = LonDec * -1
-                    
-    #                        print "Time: ", timestamp, LatDec, LonDec
-                    values = {'name':sentence[0][1:],'data':{'time':timestamp, 'lat':LatDec, 'lon':LonDec}}
-    #                        print values
+
+                    #                        print "Time: ", timestamp, LatDec, LonDec
+                    values = {'name': sentence[0][1:], 'data': {'time': timestamp, 'lat': LatDec, 'lon': LonDec}}
+                    #                        print values
                     self._callback(values)
 
 
 def testCallback(value):
     ''' callback for test, simply print content '''
-    print value
-                                   
+    print(value)
+
+
 def test():
     ''' Test function for GPS object '''
     s = GPS(testCallback)
     s.watch()
 
-if __name__=="__main__":
-    test()                                   
-                                   
+
+if __name__ == "__main__":
+    test()
